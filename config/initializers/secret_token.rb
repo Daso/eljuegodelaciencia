992652e8d21bdc294f41e4cb3ca0ed5382280dfd
Daso <daalsuol@gmail.com>
2013-04-27 20:29:00 -0500

    rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eljuegodelaciencia::Application.config.secret_token = 'da2b0c4c57bb8a4d06cfdbcb1a4b955852fb9942d908aca525449714ed1f0f51eb5cc83eb0e37f972f7b3dd190d64bac3ddf25cafe898162de5505188837fda4'
