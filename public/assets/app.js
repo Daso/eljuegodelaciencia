12f5bbe995fb9243d3030cc1433c7c428d0df16a
Daso <daalsuol@gmail.com>
2014-11-13 21:27:26 -0500

    first home

---
+++ public/assets/app.js
@@ -0,0 +1,1 @@
+(function(){jQuery("#calendar").length>0&&jQuery("#calendar").fullCalendar({events:"http://www.google.com/calendar/feeds/kfhck8hfrecneho31q1ofug8u8%40group.calendar.google.com/public/basic",dataType:"jsonp"})})(jQuery);
